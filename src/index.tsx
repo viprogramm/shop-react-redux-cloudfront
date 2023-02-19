f67beb201a1e68eaccc291a93768ba07f673e592
Serhii Hultiaieev <serhii_hultiaiev@epam.com>
2023-02-19 21:58:24 +0100

    Give a heads up to students regarding errors after the client was deployed

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,17 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
 
+// It's a common concern that failed requests in the console are a sign of
+// incorrectly deployed client.
+// In reality, requests fail because there is no API and instead client
+// is attempting to hit placeholder URLs.
+// Later, during the course you will develop APIs and direct your client to
+// hit them
+console.log(
+  '%cAttention! If you have failed network requests with status code 403 targeting URLs starting with "https://.execute-api" this is NORMAL. If you can see the UI - your site is deployed correctly. The errors will be gone as soon as you replace placeholder URLs with real back-end services.',
+  "color: red; font-size: 32px;"
+);
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: { refetchOnWindowFocus: false, retry: false, staleTime: Infinity },
