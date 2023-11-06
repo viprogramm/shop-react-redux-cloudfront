0e26cac75de6bb4fdac9d7761fb4b31ab0496f3b
Vitali Tsishkevich <vitali_tsishkevich@epam.com>
2023-11-06 22:11:27 +0300

    Task 7 (Authorization)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://fl3n8yeogi.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://wenukxushg.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://loz94zsa0g.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://4mx746f6vi.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
